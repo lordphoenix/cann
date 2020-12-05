f5edb0ab1c9849e16f287330f37798f37bd4f5fa
lordphoenix <rajmishra1995@gmail.com>
2020-12-05 18:42:13 +0530

    Add FeedForward Method

--- src/NeuralNetwork.cc
+++ src/NeuralNetwork.cc
@@ -1,12 +1,6 @@
 #include "../include/NeuralNetwork.hh"
 #include "../include/matrix.hh"
-
-void NeuralNetwork::setCurrentInput(vector<double> input){
-    this->currentInput = input;
-    for(int i=0;i<input.size();i++){
-        this->layers.at(0)->setVal(i,input.at(i));
-    }
-}
+#include "../include/utils/MultiplyMatrix.hh"
 
 NeuralNetwork::NeuralNetwork(vector<int> topology){
     this->topology = topology;
@@ -19,6 +13,29 @@ NeuralNetwork::NeuralNetwork(vector<int> topology){
     for(int i=0;i<this->topologySize-1;i++){
         Matrix *m = new Matrix(topology.at(i),topology.at(i+1),true);
         this->weightMatrices.push_back(m);
+    }
+}
+
+void NeuralNetwork::feedForward(){
+    for(int i=0;i<this->layers.size()-1;i++){
+        Matrix *a = this->getNeuronMatrix(i);
+
+        if(i!=0){
+            a = this->getActivatedNeuronMatrix(i);
+        }
+
+        Matrix *b = this->getWeightMatrix(i);
+        Matrix *c = (new utils::MultiplyMatrix(a,b))->execute();
+
+        for(int c_index=0;c_index<c->getNumCols();c_index++)
+            this->setNeuronValue(i+1,c_index,c->getValue(0,c_index));
+    }
+}
+
+void NeuralNetwork::setCurrentInput(vector<double> input){
+    this->currentInput = input;
+    for(int i=0;i<input.size();i++){
+        this->layers.at(0)->setVal(i,input.at(i));
     }
 }
 
--- src/main.cc
+++ src/main.cc
@@ -10,7 +10,7 @@ int main()
     vector<int> topology;
     topology.push_back(3);
     topology.push_back(2);
-    topology.push_back(3);
+    topology.push_back(1);
 
     vector<double> input;
     input.push_back(1.0);
@@ -19,7 +19,8 @@ int main()
 
     NeuralNetwork *nn = new NeuralNetwork(topology);
     nn->setCurrentInput(input);
-
+    nn->feedForward();
+    
     nn->printToConsole();
     return 0;
 }
