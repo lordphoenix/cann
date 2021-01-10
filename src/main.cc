6ffdb4f87a5a6b06351049ce754fe0fc78901ab3
lordphoenix <rajmishra1995@gmail.com>
2021-01-10 16:05:57 +0530

    Adding main file for build

--- src/main.cc
+++ src/main.cc
@@ -9,35 +9,49 @@
 #include "../include/utils/Math.hh"
 #include "../include/Matrix.hh"
 #include "../include/NeuralNetwork.hh"
+#include "../include/json.hpp"
+#include "../include/utils/Misc.hh"
 
 using namespace std;
+using json = nlohmann::json;
 
+void printSyntax(){
+    cerr << "Syntax :"<<endl;
+    cerr<<"cann [configFile]"<<endl;
+}
 int main(int argc, char **argv){
-    vector<double> input;
-    input.push_back(0.2);
-    input.push_back(0.5);
-    input.push_back(0.1);
 
-    vector<double> target;
-    target.push_back(0.2);
-    target.push_back(0.5);
-    target.push_back(0.1);
+    if(argc != 2){
+        printSyntax();
+        exit(-1);
+    }
 
-    double learningRate = 0.05;
-    double momentum = 1;
-    double bias = 1;
+    ifstream configFile(argv[1]);
+    string str((std::istreambuf_iterator<char>(configFile)), std::istreambuf_iterator<char>());
 
-    vector<int> topology;
-    topology.push_back(3);
-    topology.push_back(2);
-    topology.push_back(3);
+    auto config = json::parse(str);
 
-    NeuralNetwork *n = new NeuralNetwork(topology, bias, learningRate, momentum);
+    double learningRate = config["learningRate"];
+    double momentum = config["momentum"];
+    double bias = config["bias"];
+    int epoch = config["epoch"];
+    string trainingFile = config["trainingData"];
+    string labelsFile   = config["labelData"];
+
+    vector<int> topology = config["topology"];
+
+    NeuralNetwork *n = new NeuralNetwork(topology, 2, 3, 1, bias, learningRate, momentum);
     
+    vector<vector<double> > trainingData = utils::Misc::fetchData(trainingFile);
+    vector<vector<double> > labelData = utils::Misc::fetchData(labelsFile);;
     ofstream errorsOP("output.txt");
     vector<double> histErrors;
-    for(int i=0; i < 1000; i++){
-        n->train(input, target, bias, learningRate, momentum);
+    for(int i=0; i < epoch; i++){
+        for(int t_index = 0; t_index < trainingData.size(); t_index++){
+            vector<double> input = trainingData.at(t_index);
+            vector<double> target = labelData.at(t_index);
+            n->train(input, target, bias, learningRate, momentum);
+        }
         histErrors.push_back(n->error);
     }
 
